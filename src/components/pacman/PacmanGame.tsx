d46ea494a2016eded9c89af1a299d38fab4b273b
avlassov <avlassov@jmart.kz>
2025-07-13 20:13:59 +0500

    fix game iframe

--- src/components/pacman/PacmanGame.tsx
+++ src/components/pacman/PacmanGame.tsx
@@ -1,16 +1,11 @@
+import styles from "./styles/PacmanGame.module.scss";
+
 export default function PacmanIframe() {
   return (
     <iframe
       src="https://freepacman.org/"
       title="Pacman"
-      width="100%"
-      height="100%"
-      style={{
-        width: "100vw",
-        height: "100vh",
-        border: "none",
-        display: "block",
-      }}
+      className={styles.pacmanIframe}
       allowFullScreen
     />
   );
